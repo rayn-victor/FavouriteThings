f608132f47b978ff0bc44211dabda5eba1e1fe14
rayn-victor <43250423+rayn-victor@users.noreply.github.com>
2019-11-02 13:53:08 -0400

    updated views

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,13 @@ const router = express.Router();
 const sql = require('../utils/sql');
 
 router.get('/', (req, res) => {
-    
     console.log('At the home route.');
 
     let query = "SELECT ID, Name, Image, Description, Pros, Cons FROM tbl_weapons";
 
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
-        console.log(result);
+        //console.log(result);
         res.render('home', { data: result });
     })
 })
@@ -19,8 +18,15 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     console.log('At a dynamic route.');
     console.log(req.params.id)
-    
-        let query = `SELECT * FROM tbl_weapons WHERE ID="${req.params.id}"`;
+
+    //let query = "SELECT ID, Umbrellas, Pokers, Pinballs FROM tbl_models";
+    let query = `SELECT * FROM tbl_models WHERE ModelID="${req.params.id}"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
+        console.log(result);
+        res.render('models', { data: result });
+    })
 })
 
 module.exports = router;
